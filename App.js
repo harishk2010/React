42acd2d8572f907cf8e5f211d0dff75f147aad05
harish <harish200126@gmail.com>
2024-08-30 12:06:14 +0530

    30-08-2024

--- App.js
+++ App.js
@@ -1,5 +1,14 @@
-const heading = React.createElement("h1", {id:"heading"}, "Hello world , React");
+const heading = React.createElement("h1", { id: "heading" }, "Hello world , React");
 
+
+const parent = React.createElement("div", { id: "parent" },
+    [React.createElement("div", { id: "child" },
+        [React.createElement("h1", {}, "Hello im H1 tag"), React.createElement("h1", {}, "Hello im H1 tag")]
+    ),React.createElement("div", { id: "child2" },
+        [React.createElement("h2", {}, "Hello im H2 tag"), React.createElement("h2", {}, "Hello im H2 tag")]
+    )]
+)
+console.log(parent)
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-setTimeout(root.render(heading),4000)
+root.render(parent)
